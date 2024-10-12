9c26eb1bd645b5ea040456141aec017d89e0c7e1
lne <bluefoxtumbleweed@outlook.com>
2024-10-12 10:57:11 +0800

    add README

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # Querying-Cohesive-Subgraphs-in-Temporal-Graphs
+
+## Compilation Instructions
+
+Clone the repository and compile the project with `g++`:
+
+```sh
+g++ -std=c++20 -O2 src/main.cpp -o src/main
+```
+
+## Run the Program
+
+You can run the program using the following command:
+
+```sh
+./src/main --help
+```
+This will display the help menu:
+
+```
+Usage: main [-h]
+
+Pisitional arguments:
+  file          the dataset file
+
+Optional arguments:
+  -n            the number of queries to test
+```
+
+## Dataset
+
+datasets are put in data/ folder. with .data
+
+The first line should be 
+
+```
+num_time num_vert num_edge
+```
+
+follows by temporal edges
+
+```
+u1 v2 t1
+u2 v2 t2
+...
+```
+The index of verticle and timestamp should start with zero.
--- src/main.cpp
+++ src/main.cpp
@@ -1,85 +1,70 @@
 #include <algorithm>
-#include <cassert>
-#include <cstdlib>
 #include <filesystem>
 #include <fstream>
-#include <iterator>
-#include <numeric>
 #include <random>
-#include <ranges>
 
+#include "../libs/argparse.hpp"
 #include "../libs/baseline.hpp"
-#include "../libs/config.hpp"
 #include "../libs/console.hpp"
-#include "../libs/index.hpp"
+#include "../libs/graph.hpp"
 #include "../libs/orthogonal.hpp"
-// #include "../libs/priority_search_tree.hpp"
-// #include "../libs/segment_tree.hpp"
 
 int main(int argc, char *argv[])
 {
-    using namespace std::literals;
-    const auto dataset_file = std::filesystem::path{argv[1]};
-    const size_t num_query = std::stoi(argv[2]);
-    print("load data from {} and test with {} queries", dataset_file, num_query);
-    print("---------------------------------------------------------------------------");
-    auto ifs = std::ifstream{dataset_file};
-    size_type num_time = 0;
-    size_type num_vert = 0;
-    std::vector<std::tuple<size_type, size_type, size_type>> raw_data;
-    std::vector<std::tuple<size_type, size_type, size_type, size_type>>
-        temporal_edges;
-    ifs >> num_time >> num_vert >> raw_data;
-    std::ranges::transform(
-        std::move(raw_data), std::back_inserter(temporal_edges),
-        [](const std::tuple<size_type, size_type, size_type> &x) {
-            auto [u, v, t] = x;
-            return std::make_tuple(u, v, t, t);
-        });
-    print("load graph with {} verts and {} edges in [0, {})", num_vert,
-          raw_data.size(), num_time);
-    auto index = bench([&] {
-        auto data = std::map<std::pair<size_type, size_type>, std::vector<std::pair<size_type, size_type>>>{};
-        auto forest = temporal_spanning_forest(num_time, num_vert, std::move(temporal_edges));
-        print("rank is {}", forest.size());
-        for (auto &&[k, v] : forest)
-        {
-            auto [t1, t2, t3] = k;
-            assert(t1 == t2);
-            assert(t1 <= t3);
-            data[{t1, t3}] = v;
-        }
-        auto temp = std::vector<std::tuple<size_type, size_type, std::vector<std::pair<size_type, size_type>>>>{};
-        for (auto &&[k, v] : data)
-            temp.push_back(std::tuple{k.first, k.second, v});
-        print("construct");
-        return AXBY_query<size_type, std::vector<std::pair<size_type, size_type>>>{data};
-    },"compute temporal spanning forest");
+    auto args = argparse::ArgumentParser{};
+    args.add_argument("file").help("dataset path");
+    args.add_argument("-n", "--num_query").help("number of test queries").scan<'i', size_t>().default_value(10000);
+    args.parse_args(argc, argv);
+    auto file = std::filesystem::path{args.get<std::string>("file")};
+    auto num_query = args.get<size_t>("--num_query");
 
-    auto baseline_index = bench([&] {
-        auto data = std::map<std::pair<size_type, size_type>, std::vector<std::pair<size_type, size_type>>>{};
-        for (auto &&[k, v] : temporal_spanning_forest(num_time, num_vert, std::move(temporal_edges)))
+    print("running on dataset {} and test with {} queries", file, num_query);
+    auto ifs = std::ifstream{file};
+    size_t num_time, num_vert;
+    std::vector<std::tuple<size_t, size_t, int>> raw_data;
+    ifs >> num_time >> num_vert >> raw_data;
+    std::ranges::sort(raw_data);
+    auto iter = std::unique(raw_data.begin(), raw_data.end());
+    raw_data.erase(std::unique(raw_data.begin(), raw_data.end()), raw_data.end());
+    std::vector<edge_descriptor<size_t, int>> edges;
+    for (auto [u, v, t] : raw_data)
+        edges.emplace_back(u, v, t);
+
+    auto points = bench([&] {
+        return TSF_index(edges, [](auto t) { return std::pair{t, t}; });
+    },"construct TSF");
+    size_t rank = 0;
+    auto points_2d = std::map<std::array<int, 2>, std::vector<std::pair<int, int>>>{};
+    for (auto [p, v] : points)
+    {
+        rank += v.size();
+        auto [x, y, z] = p;
+        assert(x == y);
+        for (auto e : v)
         {
-            auto [t1, t2, t3] = k;
-            assert(t1 == t2);
-            data[{t1, t3}] = v;
+            points_2d[{x, z}].emplace_back(e.source_id, e.target_id);
         }
-        return baseline<std::vector<std::pair<size_type, size_type>>>{static_cast<size_t>(num_time), data};
-    },"construct baseline index");
+    }
+    print("the rank is {}", rank);
+    auto index = bench([&] {
+        return AXBY_query<int, std::vector<std::pair<int, int>>>{points_2d};
+    },"construct index");
+    auto baseline_index = baseline<std::vector<std::pair<int, int>>>{static_cast<size_t>(num_time), points_2d};
 
     for (auto ratio : {0.2, 0.4, 0.6, 0.8})
     {
         print("[[ratio = {}]]", ratio);
-        auto queries = std::vector<std::pair<size_type, size_type>>(num_query);
+        auto queries = std::vector<std::pair<int, int>>(num_query);
         auto random_engine = std::default_random_engine(std::random_device{}());
-        size_type length = ratio * num_time;
-        auto distrib = std::uniform_int_distribution<size_type>(0, num_time - length - 1);
+        int length = ratio * num_time;
+        auto distrib = std::uniform_int_distribution<int>(0, num_time - length - 1);
         std::ranges::generate(queries, [=, &random_engine, &distrib] {
             auto ts = distrib(random_engine);
             return std::make_pair(ts, ts + length);
         });
         std::vector<size_type> result1(num_query);
         std::vector<size_type> result2(num_query);
+        std::vector<size_type> result3(num_query);
 
         bench([&] {
             for (size_t i = 0; i < num_query; i++)
@@ -90,8 +75,9 @@ int main(int argc, char *argv[])
                         result1[i]++;
                 });
             }
-        },"solve query");
-        bench( [&] {
+        },
+              "solve query");
+        bench([&] {
             for (size_t i = 0; i < num_query; i++)
             {
                 auto [ts, te] = queries[i];
@@ -101,6 +87,15 @@ int main(int argc, char *argv[])
                 });
             }
         },"baseline query");
-        print("result1 = result2 ? {}", result1 == result2);
+        if (result1 != result2)
+        {
+            for (size_t i = 0; i < num_query; i++)
+            {
+                if (result1[i] != result2[i])
+                {
+                    print("result 1 = {}, result 2 = {}, at {}", result1[i], result2[i], queries[i]);
+                }
+            }
+        }
     }
 }
