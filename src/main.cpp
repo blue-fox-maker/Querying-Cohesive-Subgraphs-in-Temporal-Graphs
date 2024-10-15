0b2a1ad0ba26d811ffd6a256a23ffa55f2dcc213
lne <bluefoxtumbleweed@outlook.com>
2024-10-15 11:55:41 +0800

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,10 @@
 #include <fstream>
 #include <random>
 
-#include "../libs/argparse.hpp"
-#include "../libs/baseline.hpp"
-#include "../libs/console.hpp"
-#include "../libs/graph.hpp"
-#include "../libs/orthogonal.hpp"
+#include "../include/argparse.hpp"
+#include "../include/console.hpp"
+#include "../include/orthogonal.hpp"
+#include "../include/star.hpp"
 
 int main(int argc, char *argv[])
 {
@@ -23,79 +22,9 @@ int main(int argc, char *argv[])
     size_t num_time, num_vert;
     std::vector<std::tuple<size_t, size_t, int>> raw_data;
     ifs >> num_time >> num_vert >> raw_data;
-    std::ranges::sort(raw_data);
+    auto edges = std::map<std::pair<size_t,size_t>,std::vector<int>>{};
+    for(auto [u,v,t]:raw_data)
+        edges[{u,v}].push_back(t);
     auto iter = std::unique(raw_data.begin(), raw_data.end());
     raw_data.erase(std::unique(raw_data.begin(), raw_data.end()), raw_data.end());
-    std::vector<edge_descriptor<size_t, int>> edges;
-    for (auto [u, v, t] : raw_data)
-        edges.emplace_back(u, v, t);
-
-    auto points = bench([&] {
-        return TSF_index(edges, [](auto t) { return std::pair{t, t}; });
-    },"construct TSF");
-    size_t rank = 0;
-    auto points_2d = std::map<std::array<int, 2>, std::vector<std::pair<int, int>>>{};
-    for (auto [p, v] : points)
-    {
-        rank += v.size();
-        auto [x, y, z] = p;
-        assert(x == y);
-        for (auto e : v)
-        {
-            points_2d[{x, z}].emplace_back(e.source_id, e.target_id);
-        }
-    }
-    print("the rank is {}", rank);
-    auto index = bench([&] {
-        return AXBY_query<int, std::vector<std::pair<int, int>>>{points_2d};
-    },"construct index");
-    auto baseline_index = baseline<std::vector<std::pair<int, int>>>{static_cast<size_t>(num_time), points_2d};
-
-    for (auto ratio : {0.2, 0.4, 0.6, 0.8})
-    {
-        print("[[ratio = {}]]", ratio);
-        auto queries = std::vector<std::pair<int, int>>(num_query);
-        auto random_engine = std::default_random_engine(std::random_device{}());
-        int length = ratio * num_time;
-        auto distrib = std::uniform_int_distribution<int>(0, num_time - length - 1);
-        std::ranges::generate(queries, [=, &random_engine, &distrib] {
-            auto ts = distrib(random_engine);
-            return std::make_pair(ts, ts + length);
-        });
-        std::vector<size_type> result1(num_query);
-        std::vector<size_type> result2(num_query);
-        std::vector<size_type> result3(num_query);
-
-        bench([&] {
-            for (size_t i = 0; i < num_query; i++)
-            {
-                auto [ts, te] = queries[i];
-                index.query(ts, te, [&](const auto &point) {
-                    for (auto &&val : point)
-                        result1[i]++;
-                });
-            }
-        },
-              "solve query");
-        bench([&] {
-            for (size_t i = 0; i < num_query; i++)
-            {
-                auto [ts, te] = queries[i];
-                baseline_index.solve(ts, te, [&](const auto &point) {
-                    for (auto &&val : point)
-                        result2[i]++;
-                });
-            }
-        },"baseline query");
-        if (result1 != result2)
-        {
-            for (size_t i = 0; i < num_query; i++)
-            {
-                if (result1[i] != result2[i])
-                {
-                    print("result 1 = {}, result 2 = {}, at {}", result1[i], result2[i], queries[i]);
-                }
-            }
-        }
-    }
 }
